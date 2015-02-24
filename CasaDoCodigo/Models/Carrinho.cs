b2407095808d9a943443c881f01e3a024381c9b5
bigodera <developer.gabriel@gmail.com>
2015-02-24 18:06:48 -0300

    Adicionando umas coisinhas

---
+++ CasaDoCodigo/Models/Carrinho.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CasaDoCodigo.Models
+{
+    public class Carrinho
+    {
+        public SortedSet<ItemCompra> Itens { get; set; }
+    }
+}
