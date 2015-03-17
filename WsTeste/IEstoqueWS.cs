7d50aa9b9ecd77b866863c20aa09c2a82317f09c
Gabriel <developer.gabriel@gmail.com>
2015-03-17 13:58:36 -0300

    Aqui tem o primeiro servicinho pronto pra testar com SoapUI

--- WsTeste/IEstoqueWS.cs
+++ WsTeste/IEstoqueWS.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
 
-namespace ServicoEstoque
+namespace fn34_webservice
 {
     [ServiceContract]
     public interface IEstoqueWS
     {
+        //[OperationContract]
+        //ItemEstoque GetQuantidade(string codigo);
+
         [OperationContract]
-        ItemEstoque GetQuantidade(string codigo);
+        string Teste();
     }
 }
--- fn34-webservice/EstoqueWS.cs
+++ fn34-webservice/EstoqueWS.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fn34_webservice
+namespace ServicoEstoque
 {
     public class EstoqueWS : IEstoqueWS
     {
