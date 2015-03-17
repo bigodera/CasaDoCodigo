47d77e2309852fb931e8d90944caede263bc7acf
Gabriel <developer.gabriel@gmail.com>
2015-03-17 12:41:07 -0300

    Adicionando o projeto do fn34-webservice, aqui já fiz o serviço, as classes e tal, só falta hostear no iis express

---
+++ fn34-webservice/EstoqueWS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fn34_webservice
+{
+    public class EstoqueWS : IEstoqueWS
+    {
+        private Dictionary<string, ItemEstoque> repositorio = new Dictionary<string, ItemEstoque>();
+
+        public EstoqueWS()
+        {
+            repositorio.Add("SOA", new ItemEstoque() { Codigo = "SOA", Quantidade = 5 });
+            repositorio.Add("TDD", new ItemEstoque() { Codigo = "TDD", Quantidade = 1 });
+            repositorio.Add("RES", new ItemEstoque() { Codigo = "RES", Quantidade = 2 });
+            repositorio.Add("LOG", new ItemEstoque() { Codigo = "LOG", Quantidade = 4 });
+            repositorio.Add("WEB", new ItemEstoque() { Codigo = "WEB", Quantidade = 1 });
+            repositorio.Add("ARQ", new ItemEstoque() { Codigo = "ARQ", Quantidade = 2 });
+        }
+
+        public ItemEstoque GetQuantidade(string codigo)
+        {
+            return repositorio[codigo];
+        }
+    }
+}
---
+++ fn34-webservice/ItemEstoque.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fn34_webservice
+{
+    public class ItemEstoque
+    {
+        public string Codigo { get; set; }
+        public int Quantidade { get; set; }
+
+        public ItemEstoque()
+        {
+
+        }
+
+        public ItemEstoque(string codigo, int quantidade)
+        {
+            this.Codigo = codigo;
+            this.Quantidade = quantidade;
+        }
+    }
+}
